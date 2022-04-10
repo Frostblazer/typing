11a9fa366929b76ff3b1ecc802765d3082d7e7d8
Shawn Witemyre <switemyre@comcast.net>
2022-04-10 00:58:49 -0400

    Added random string, comparison of random string and user string, styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  userFinalInput = '';
+
+  onChangeText(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const userInput = target.value;
+    this.userFinalInput = userInput;
+
+    console.log(userInput.split(''));
+    // console.log(userInput);
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
